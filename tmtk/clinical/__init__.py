9ab00f121b34c8fa8b90aef8c0defe72171753dd
jochemb <j.bijlard@gmail.com>
2016-04-11 12:44:29 +0200

    Revert "Fix for casing issue"
    This reverts commit 972c1654dc59ad121bd90065b6c12897fc76db97.

---
+++ tmtk/clinical/__init__.py
@@ -0,0 +1,18 @@
+from .DataFile import *
+from .Variable import *
+from .ColumnMapping import *
+from .WordMapping import *
+import tmtk.utils as utils
+
+
+class Clinical:
+    """
+    Container class for all clinical information.
+    """
+    def __init__(self, clinical_params):
+        self.ColumnMapping = ColumnMapping(params=clinical_params)
+        self.WordMapping = WordMapping(params=clinical_params)
+
+        for file in self.ColumnMapping.included_datafiles():
+            safe_name = utils.clean_for_namespace(file)
+            self.__dict__[safe_name] = DataFile(path=os.path.join(clinical_params.dirname, file))
