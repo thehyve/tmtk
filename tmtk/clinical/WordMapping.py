67a9774c36db76b8145188797e7c694780fb3055
jochemb <j.bijlard@gmail.com>
2016-05-15 09:19:08 +0200

    Nicely create column mapping if it does not exist.

--- tmtk/clinical/WordMapping.py
+++ tmtk/clinical/WordMapping.py
@@ -1,6 +1,8 @@
 from tmtk import utils
 import os
-
+from tmtk.utils import CPrint
+import pandas as pd
+from tmtk.params import ClinicalParams
 
 
 class WordMapping:
@@ -8,15 +10,29 @@ class WordMapping:
     Base Class for word mapping file.
     """
     def __init__(self, params=None):
-        # Todo improve this check here.
-        if params and params.__dict__.get('WORD_MAP_FILE'):
+        if not isinstance(params, ClinicalParams):
+            raise utils.Exceptions.ClassError(type(params))
+        elif params.__dict__.get('WORD_MAP_FILE'):
             self.path = os.path.join(params.dirname, params.WORD_MAP_FILE)
         else:
-            raise utils.Exceptions.ClassError(type(params), tmtk.params.ClinicalParams)
+            self.path = os.path.join(params.dirname, 'word_mapping_file.txt')
+            self.params['WORD_MAP_FILE'] = os.path.basename(self.path)
+
+        self._hash_init = None
+        self.params = params
 
     @utils.cached_property
     def df(self):
-        return utils.file2df(self.path)
+        if self.path and os.path.exists(self.path):
+            df, self._hash_init = utils.file2df(self.path, hashed=True)
+        else:
+            CPrint.warn("No word mapping detected, creating.")
+            df = self.create_wordmapping_file()
+        return df
+
+    @property
+    def _hash(self):
+        return hash(self.df.__bytes__())
 
     def validate(self, verbosity=2):
         pass
@@ -37,3 +53,11 @@ class WordMapping:
             # fill dict with col3 as key and col4 as value
             self.df.ix[f & c].apply(lambda x: mapping_dict.update({x[2]: x[3]}), axis=1)
         return mapping_dict
+
+    @staticmethod
+    def create_wordmapping_file():
+        df = pd.DataFrame(dtype=str, columns=['Filename',
+                                              'Column Number',
+                                              'Datafile Value',
+                                              'Mapping Value'])
+        return df
