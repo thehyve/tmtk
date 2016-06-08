3d223da1457fc930f08425d907030b61d8518563
jochemb <j.bijlard@gmail.com>
2016-06-08 23:40:23 +0200

    Update study from tree file

--- tmtk/arborist/__init__.py
+++ tmtk/arborist/__init__.py
@@ -1,3 +1,3 @@
 from .jstreecontrol import create_concept_tree, ConceptTree, create_tree_from_study
-from .common import call_boris, get_open_port
+from .common import call_boris, get_open_port, update_study_from_json
 from .flask_connection import app
