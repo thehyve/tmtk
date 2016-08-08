c786b2ab561161f645223d2519aef8a1cd0c3412
jochemb <j.bijlard@gmail.com>
2016-08-08 23:58:01 +0200

    only load rpy2 if needed, as it is slow.

--- tmtk/toolbox/generate_chromosomal_regions_file.py
+++ tmtk/toolbox/generate_chromosomal_regions_file.py
@@ -1,5 +1,3 @@
-import rpy2.robjects as robjects
-import rpy2.robjects.pandas2ri as pandas2ri
 import os
 
 
@@ -11,6 +9,9 @@ def generate_chromosomal_regions_file(platform_id=None, reference_build='hg19', 
     :param reference_build: choose either hg18, hg19 or hg38
     :return: a pandas dataframe with the new platform
     """
+    import rpy2.robjects as robjects
+    import rpy2.robjects.pandas2ri as pandas2ri
+
     host_mapping = {'hg18': 'may2009.archive.ensembl.org',
                     'hg19': 'grch37.ensembl.org',
                     'hg38': 'www.ensembl.org'}
