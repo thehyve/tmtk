c451e96339c44da9580175894f2c734b9b7ef980
jochemb <j.bijlard@gmail.com>
2016-08-12 16:52:07 +0200

    requirements for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['rpy2']
+MOCK_MODULES = ['rpy2', 'pandas', 'numpy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
