a75d81af10f11e44fa60e800efb65b68dd18dc03
jochemb <j.bijlard@gmail.com>
2016-10-19 14:36:46 +0200

    Set IFrame width to 100% instead of fixed

--- tmtk/arborist/common.py
+++ tmtk/arborist/common.py
@@ -69,7 +69,7 @@ def launch_arborist_gui(json_data: str):
     # Add wait for 0.5 second to give flask time to launch
     time.sleep(0.25)
 
-    display(IFrame(src=running_on, width=950, height=500))
+    display(IFrame(src=running_on, width='100%', height=500))
 
     # Wait for the GUI app to be killed by user button input
     app_thread.join()
