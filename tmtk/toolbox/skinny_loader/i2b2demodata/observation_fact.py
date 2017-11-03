984386cc8b30b4a3a0ef26715e9ef61db28cc78d
jochemb <j.bijlard@gmail.com>
2017-11-03 12:43:08 +0100

    Optimizations for observation_fact to get allow it to finish within the life time of the universe for reasonably sized data.

--- tmtk/toolbox/skinny_loader/i2b2demodata/observation_fact.py
+++ tmtk/toolbox/skinny_loader/i2b2demodata/observation_fact.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 
 
 class ObservationFact(TableRow):
-
     def __init__(self, skinny, straight_to_disk=False):
 
         self.skinny = skinny
@@ -21,13 +20,13 @@ class ObservationFact(TableRow):
             self.write_to_disk(straight_to_disk)
 
     def _build_in_memory(self):
-        rows = []
 
+        dfs = []
         # Loop through all variables in the clinical data and add a row
         for variable in tqdm(self.skinny.study.Clinical.filtered_variables.values()):
-            rows += [r for r in self.build_rows(variable)]
+            dfs += [r for r in self.build_rows(variable)]
 
-        self.df = pd.DataFrame(rows, columns=self.columns)
+        self.df = pd.concat(dfs, ignore_index=True)
 
     def write_to_disk(self, path):
         with open(path, 'w') as f:
@@ -41,7 +40,8 @@ class ObservationFact(TableRow):
         """
         Returns all observation fact rows for a given variable.
         """
-        def set_value_fields(row_, value_, visual_attributes):
+
+        def get_value_fields(values, visual_attributes):
             """
             Update a row object with its value based on visual_attributes
 
@@ -51,27 +51,25 @@ class ObservationFact(TableRow):
             :return: updated row.
             """
             if visual_attributes == var.VIS_DATE:
-                row_.valtype_cd = 'N'
-                row_.tval_char = 'E'
-                # Unix time
-                row_.nval_num = arrow.get(value_).format('X')
-                # UTC
-                row_.observation_blob = value_
+                return {'valtype_cd': 'N',
+                        'tval_char': 'E',
+                        # Unix time
+                        'nval_num': values.apply(lambda x: arrow.get(x).format('X')),
+                        # UTC
+                        'observation_blob': values}
 
             elif visual_attributes == var.VIS_TEXT:
-                row_.valtype_cd = 'B'
-                row_.observation_blob = value_
+                return {'valtype_cd': 'B',
+                        'observation_blob': values}
 
             elif visual_attributes == var.VIS_NUMERIC:
-                row_.valtype_cd = 'N'
-                row_.tval_char = 'E'
-                row_.nval_num = value_
+                return {'valtype_cd': 'N',
+                        'tval_char': 'E',
+                        'nval_num': values}
 
             elif visual_attributes == var.VIS_CATEGORICAL:
-                row_.valtype_cd = 'T'
-                row_.tval_char = value_
-
-            return row_
+                return {'valtype_cd': 'T',
+                        'tval_char': values}
 
         # Preload these, so we don't have to get them for every value in the current variable
         modifiers = var.modifiers
@@ -80,37 +78,49 @@ class ObservationFact(TableRow):
         trial_visit = var.trial_visit
         visual_attributes = var.visual_attributes
 
-        concept_cd = get_concept_identifier(var, self.skinny.study)
+        var_wide_data = {
+            'encounter_num': -1,
+            'patient_num': var.subj_id.values.apply(lambda x: self.skinny.patient_mapping.map[x]),
+            'concept_cd': get_concept_identifier(var, self.skinny.study),
+            'provider_id': '@',
+            'start_date': start_date.values[i] if start_date else None,
+            'modifier_cd': '@',
+            'instance_num': 1}
+        var_wide_data.update(get_value_fields(var.values, visual_attributes))
 
-        for i, value in enumerate(var.values):
+        df = pd.DataFrame(var_wide_data, columns=self.columns)
+        yield df
 
-            if self.not_a_value(value):
-                # Check if there are any modifier values for this 'normal' values
-                # We want to insert an 'empty' normal observation if there are modified observations.
-                # This is useful to annotated missing values.
-                if all([self.not_a_value(mod.values[i]) for mod in modifiers]):
-                    continue
+    #         for i, (value, subj_id_value) in enumerate(zip(var.values, subj_id.values)):
 
-            base_row = self.row
-            base_row.patient_num = self.skinny.patient_mapping.map[subj_id.values[i]]
-            base_row.concept_cd = concept_cd
-            base_row.start_date = start_date.values[i] if start_date else None
-            # trial visits other than 'General' are currently not supported
-            base_row.trial_visit_num = self.skinny.trial_visit_dimension.map[trial_visit]
+    #             if self.not_a_value(value):
+    #                 # Check if there are any modifier values for this 'normal' values
+    #                 # We want to insert an 'empty' normal observation if there are modified observations.
+    #                 # This is useful to annotated missing values.
+    #                 if all([self.not_a_value(mod.values[i]) for mod in modifiers]):
+    #                     continue
 
-            yield set_value_fields(base_row.copy(), value, visual_attributes)
+    #             base_row = self.row
+    #             base_row.patient_num = self.skinny.patient_mapping.map[subj_id_value]
 
-            for modifier_variable in modifiers:
+    #             base_row.concept_cd = concept_cd
+    #             base_row.start_date = start_date.values[i] if start_date else None
+    #             # trial visits other than 'General' are currently not supported
+    #             base_row.trial_visit_num = self.skinny.trial_visit_dimension.map[trial_visit]
 
-                modifier_value = modifier_variable.values[i]
+    #             yield set_value_fields(base_row.copy(), value, visual_attributes)
 
-                if self.not_a_value(modifier_value):
-                    continue
+    #             for modifier_variable in modifiers:
 
-                modifier_row = base_row.copy()
-                modifier_row.modifier_cd = modifier_variable.modifier_code
+    #                 modifier_value = modifier_variable.values[i]
 
-                yield set_value_fields(modifier_row, modifier_value, modifier_variable.visual_attributes)
+    #                 if self.not_a_value(modifier_value):
+    #                     continue
+
+    #                 modifier_row = base_row.copy()
+    #                 modifier_row.modifier_cd = modifier_variable.modifier_code
+
+    #                 yield set_value_fields(modifier_row, modifier_value, modifier_variable.visual_attributes)
 
     @staticmethod
     def not_a_value(value) -> bool:
@@ -120,13 +130,13 @@ class ObservationFact(TableRow):
     def _row_definition(self):
         return pd.Series(
             data=[
-                -1,    # encounter_num
+                -1,  # encounter_num
                 None,  # patient_num
                 None,  # concept_cd
-                '@',   # provider_id
+                '@',  # provider_id
                 None,  # start_date
-                '@',   # modifier_cd
-                1,     # instance_num
+                '@',  # modifier_cd
+                1,  # instance_num
                 None,  # trial_visit_num
                 None,  # valtype_cd
                 None,  # tval_char
