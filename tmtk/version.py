b65d4df3b796863ce3d67ff1209aec0afd623665
jochemb <j.bijlard@gmail.com>
2017-12-22 11:10:10 +0100

    Bump to 0.4.1

---
+++ tmtk/version.py
@@ -0,0 +1,8 @@
+"""
+Copyright (c) 2017  The Hyve B.V.
+This file is distributed under the GNU General Public License
+  (see accompanying file LICENSE).
+"""
+
+version_info = (0, 4, 1, 'dev1')
+__version__ = '.'.join(map(str, version_info))
