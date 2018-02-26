e271244d4b86adbd642e3a5b89ad433b6b7d430e
wibo-hyve <wibo@thehyve.nl>
2018-02-26 10:06:44 +0100

    Extended unittest coverage

--- tests/template_reader_tests.py
+++ tests/template_reader_tests.py
@@ -4,16 +4,19 @@ import os
 
 from tmtk.toolbox.template_reader.sheets import TreeSheet, ModifierSheet, TrialVisitSheet, BlueprintFile, \
     ValueSubstitutionSheet
-from tmtk.toolbox.template_reader.create_study_from_templates import COMMENT, EXPECTED_SHEETS, get_template_sheets
+from tmtk.toolbox.template_reader.create_study_from_templates import COMMENT, EXPECTED_SHEETS, get_template_sheets, \
+    template_reader
 from tmtk.utils.mappings import Mappings
-from tmtk.toolbox.template_reader.sheet_exceptions import ValueSubstitutionError, MetaDataException
+from tmtk.toolbox.template_reader.sheet_exceptions import ValueSubstitutionError, MetaDataException, TemplateException
 from tmtk.study import Study
+
 
 class TemplateReaderTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.template_dir = os.path.join('studies', 'template_reader')
-        cls.template = pd.ExcelFile('studies/template_reader/Clinical_data_2017_mock_study_V2.xlsx')
+        cls.template_file = 'studies/template_reader/Clinical_data_2017_mock_study_V2.xlsx'
+        cls.template = pd.ExcelFile(cls.template_file)
+        cls.incorrect_template = pd.ExcelFile('studies/template_reader/Clinical_data_2017_mock_study_errors.xlsx')
         cls.study = Study()
 
     @classmethod
@@ -27,57 +30,65 @@ class TemplateReaderTests(unittest.TestCase):
         pass
 
     def test_get_template_sheets(self):
-        pass
+        t, m, vs, tv = get_template_sheets(self.template)
+        self.assertIsInstance(t, TreeSheet)
+        self.assertIsInstance(m, ModifierSheet)
+        self.assertIsInstance(vs, ValueSubstitutionSheet)
+        self.assertIsInstance(tv, TrialVisitSheet)
+
+    def test_get_template_sheets_error(self):
+        self.assertRaises(TemplateException, get_template_sheets, self.incorrect_template)
 
     def test_tree_sheet(self):
         tree_sheet = TreeSheet(self.template.parse(EXPECTED_SHEETS['TreeSheet'], comment=COMMENT))
-        assert tree_sheet.data_sources == ['Low-dimensional data (Mock)', 'SAMPLE (Mock)', 'TRIAL_VISIT_Data (Mock)']
-        assert tree_sheet.df.shape == (25, 12)
+        self.assertListEqual(tree_sheet.data_sources,
+                             ['Low-dimensional data (Mock)', 'SAMPLE (Mock)', 'TRIAL_VISIT_Data (Mock)'])
+        self.assertTupleEqual(tree_sheet.df.shape, (25, 12))
         l = [l for l in tree_sheet.get_meta_columns_iter()]
         mt_l = [('Level 4 metadata tag', 'Level 4 metadata value'), ('Level 5 metadata tag', 'Level 5 metadata value')]
-        assert l == mt_l
+        self.assertListEqual(l, mt_l)
         tags_df = tree_sheet.create_metadata_tags_file()
-        assert tags_df.shape == (7, 4)
-        assert tags_df.columns.tolist() == Mappings.tags_header
+        self.assertTupleEqual(tags_df.shape, (7, 4))
+        self.assertListEqual(tags_df.columns.tolist(), Mappings.tags_header)
 
     def test_meta_tags_fail(self):
-        tree_sheet = TreeSheet(self.template.parse(EXPECTED_SHEETS['TreeSheet'], comment=COMMENT).iloc[:,:-1])
+        tree_sheet = TreeSheet(self.template.parse(EXPECTED_SHEETS['TreeSheet'], comment=COMMENT).iloc[:, :-1])
         self.assertRaises(MetaDataException, tree_sheet.create_metadata_tags_file)
-
 
     def test_modifier_sheet(self):
         modifier_sheet = ModifierSheet(self.template.parse(EXPECTED_SHEETS['ModifierSheet'], comment=COMMENT))
-        assert modifier_sheet.df.shape == (3,4)
-        assert modifier_sheet.modifier_blueprint == {}
+        self.assertTupleEqual(modifier_sheet.df.shape, (3, 4))
+        self.assertDictEqual(modifier_sheet.modifier_blueprint, {})
 
         modifier_sheet._set_initial_modifier_blueprint(self.study.Clinical.Modifiers.df)
-        assert 'SAMPLE_ID' in modifier_sheet.modifier_blueprint
+        self.assertIn('SAMPLE_ID', modifier_sheet.modifier_blueprint)
 
         modifier_sheet.update_modifier_blueprint('Blood@UNIT')
-        assert 'Blood@UNIT' in modifier_sheet.modifier_blueprint
+        self.assertIn('Blood@UNIT', modifier_sheet.modifier_blueprint)
         reference_column = 'Blood'
-        data_type= 'UNIT'
-        assert modifier_sheet.modifier_blueprint['Blood@UNIT'] == {'label': 'MODIFIER',
-                                                                   'data_type': data_type,
-                                                                   'reference_column': reference_column}
+        data_type = 'UNIT'
+        self.assertDictEqual(modifier_sheet.modifier_blueprint['Blood@UNIT'], {'label'           : 'MODIFIER',
+                                                                               'data_type'       : data_type,
+                                                                               'reference_column': reference_column})
 
     def test_value_substitution_sheet(self):
-        value_substitution_sheet = ValueSubstitutionSheet(self.template.parse(EXPECTED_SHEETS['ValueSubstitutionSheet'], comment=COMMENT))
-        assert value_substitution_sheet.df.shape == (6, 4)
+        value_substitution_sheet = ValueSubstitutionSheet(
+            self.template.parse(EXPECTED_SHEETS['ValueSubstitutionSheet'], comment=COMMENT))
+        self.assertTupleEqual(value_substitution_sheet.df.shape, (6, 4))
         key_set = {'Gender', 'QLQ-C30_Q01', 'QLQ-C30_Q02', 'QLQ-C30_Q03', 'QLQ-C30_Q04'}
-        assert set(value_substitution_sheet.word_map.keys()) == key_set
+        self.assertSequenceEqual(value_substitution_sheet.word_map.keys(), key_set)
 
     def test_trial_visit_sheet(self):
         trial_visit_sheet = TrialVisitSheet(self.template.parse(EXPECTED_SHEETS['TrialVisitSheet'], comment=COMMENT))
-        assert trial_visit_sheet.df.shape == (7, 3)
-        assert trial_visit_sheet.df.columns.tolist() == Mappings.trial_visits_header
+        self.assertTupleEqual(trial_visit_sheet.df.shape, (7, 3))
+        self.assertListEqual(trial_visit_sheet.df.columns.tolist(), Mappings.trial_visits_header)
 
     def test_trial_visit_sheet_exception(self):
         value_df = pd.DataFrame.from_dict({
             'column name'         : {
                 0: 'QLQ-C30_Q04',
                 1: 'QLQ-C30_Q04'},
-            'from value' : {
+            'from value'          : {
                 0: '4',
                 1: '4'},
             'sheet name/file name': {
@@ -92,6 +103,57 @@ class TemplateReaderTests(unittest.TestCase):
     def test_blue_print_file(self):
         tree_sheet = TreeSheet(self.template.parse(EXPECTED_SHEETS['TreeSheet'], comment=COMMENT))
         b = BlueprintFile(tree_sheet)
-        assert 'Blood_Volume' in b.blueprint
-        assert len(b.blueprint) == 29
-        assert {'label', 'path'}.issubset(b.blueprint['Blood_Volume'])
+        self.assertIn('Blood_Volume', b.blueprint)
+        self.assertEqual(len(b.blueprint), 29)
+        self.assertDictEqual(
+            {
+                'label': 'Blood volume',
+                'path' : '3. Biobank\\Blood'
+            },
+            b.blueprint['Blood_Volume']
+        )
+
+        update_item = {'Blood_Volume':
+            {
+                'label'   : 'Volume of blood',
+                'path'    : '3. Biobank',
+                'new_item': 'test'
+            }
+        }
+        b.update_blueprint_item(update_item)
+        self.assertDictEqual(
+            {
+                'label'   : 'Volume of blood',
+                'path'    : '3. Biobank',
+                'new_item': 'test'
+            },
+            b.blueprint['Blood_Volume']
+        )
+
+        new_item = {'test_item':
+            {
+                'label': 'this is a test',
+                'path' : 'some path',
+            }
+        }
+
+        b.update_blueprint(new_item)
+        self.assertDictEqual(
+            {
+                'label': 'this is a test',
+                'path' : 'some path',
+            }, b.blueprint['test_item']
+        )
+
+    def test_template_reader(self):
+        study = template_reader(self.template_file)
+        col_map = study.Clinical.ColumnMapping.df
+        modifiers = study.Clinical.Modifiers.df
+        self.assertTupleEqual(col_map.shape, (39,7))
+
+
+        used_modifiers = col_map.iloc[:,-1].unique().tolist()
+        [self.assertIn(item, modifiers.modifier_cd.unique().tolist()+[''])
+         for item in used_modifiers]
+
+
