1d4cc7f075037f9d959eb5cd95da44ee8887219f
jochemb <j.bijlard@gmail.com>
2018-03-04 10:15:04 +0100

    Added license to manifest and fixed ci build.

--- tmtk/version.py
+++ tmtk/version.py
@@ -4,5 +4,5 @@ This file is distributed under the GNU General Public License
   (see accompanying file LICENSE).
 """
 
-version_info = (0, 4, 4, 'dev1')
+version_info = (0, 4, 4, 'dev3')
 __version__ = '.'.join(map(str, version_info))
