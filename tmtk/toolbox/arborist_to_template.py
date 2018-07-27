32c62a701ca6e26f3b32127fc03bcaca0784ae47
Spayralbe <stefan@thehyve.nl>
2018-07-27 16:05:32 +0200

    Expand documentation and add option for df retrieval before writing

--- tmtk/toolbox/arborist_to_template.py
+++ tmtk/toolbox/arborist_to_template.py
@@ -7,22 +7,24 @@ import pandas as pd
 from tmtk import arborist
 
 
-def arborist_to_tree_template(tree_input, output_dir='.', no_metadata=False):
+def arborist_to_tree_template(tree_input, output_dir='.', no_metadata=False, write_immediately=False):
     """
-    Create an Excel file containing the tree structure template
+    Create a df containing the tree structure template
     based on the JSON of an Arborist tree.
     :param tree_input: An exported treefile or an Arborist tree URL
     :param output_dir: The folder where the output will be written
     :param no_metadata: if True metadata will be ignored
+    :param write_immediately: write resulting template to Excel file, otherwise return the df
+    :return ArboristToTemplate instance
     """
     json_tree, study_name = _interpret_input(tree_input)
-    tree = _ArboristToTemplate(json_tree, study_name, output_dir, no_metadata)
-    tree.transform_to_template()
+    tree = _ArboristToTemplate(json_tree, study_name, output_dir, no_metadata, write_immediately)
     print("Processing complete")
+    return tree
 
 
 def _interpret_input(tree_input):
-    if tree_input.startswith('http'):
+    if tree_input.lower().startswith('http'):
         json_tree = _get_json_tree_from_url(tree_input)
         study_name = _get_study_name_from_url(tree_input)
     else:
@@ -33,7 +35,7 @@ def _interpret_input(tree_input):
 
 
 def _get_study_name_from_url(tree_input_url):
-    # Name of study is the URL part after "trees"
+    """Name of study is the URL part after 'trees'"""
     parts = tree_input_url.split('/')
     trees_index = parts.index('trees')
     study_name = parts[trees_index+1]
@@ -59,43 +61,55 @@ def _read_json_file(file_path):
 
 class _ArboristToTemplate:
 
-    def __init__(self, json_tree, study_name, output_dir='.', no_metadata=False):
+    def __init__(self, json_tree, study_name, output_dir='.', no_metadata=False, write_immediately=False):
         self.raw_tree = json_tree
         self.study_name = study_name
         self.output_path = os.path.join(output_dir, self.study_name+'_as_template.xlsx')
         self.skip_metadata = no_metadata
-
+        self._write_immediately = write_immediately
         self.df = pd.DataFrame()
 
-    def transform_to_template(self):
-        self.add_root_to_json_tree()
-        self.loop_through_nodes(self.raw_tree)
-        self.add_missing_metadata_columns()
-        self.order_columns()
-        self.add_mandatory_columns()
+        self._transform_to_template()
 
+    def _transform_to_template(self):
+        self._add_root_to_json_tree()
+        self._loop_through_nodes(self.raw_tree)
+        self._add_missing_metadata_columns()
+        self._order_columns()
+        self._add_mandatory_columns()
+
+        if self._write_immediately:
+            self.write_tree_template_df_to_excel()
+
+    def write_tree_template_df_to_excel(self):
+        """
+        Write the tree template df to Excel file as defined in output_path
+        :return: None
+        """
         writer = pd.ExcelWriter(self.output_path)
         self.df.to_excel(writer, sheet_name='Tree structure template', index=False)
         writer.save()
         print("Template written at: {}".format(self.output_path))
 
-    def add_root_to_json_tree(self):
+    def _add_root_to_json_tree(self):
+        """Add a node to the raw JSON tree representing the top node and give it the name of the study.
+        Otherwise the raw JSON tree has multiple root nodes and this makes processing awkward."""
         full_tree = {'id': 'root', 'text': self.study_name, 'data': {}, 'children': self.raw_tree}
         self.raw_tree = full_tree
 
-    def add_mandatory_columns(self):
+    def _add_mandatory_columns(self):
         self.df.insert(loc=0, column='Column number', value=np.nan)
         self.df.insert(loc=0, column='Sheet name/File name', value=np.nan)
         self.df.insert(loc=0, column='tranSMART data type', value='Low-dimensional')
 
-    def order_columns(self):
+    def _order_columns(self):
         level_order = ['Level {}', 'Level {} metadata tag', 'Level {} metadata value']
-        max_level = max(
-            [int(col.split()[1]) for col in self.df.columns if col.startswith('Level') and not 'metadata' in col])
+        max_level = max([int(col.split()[1]) for col in self.df.columns if col.startswith('Level')
+                         and 'metadata' not in col])
         ordered_cols = [col.format(i) for i in range(1, max_level + 1) for col in level_order]
         self.df = self.df.reindex(ordered_cols, axis=1)
 
-    def add_missing_metadata_columns(self):
+    def _add_missing_metadata_columns(self):
         levels = {col.split()[1] for col in self.df.columns}
         for level in levels:
             if 'Level {} metadata tag'.format(level) not in self.df.columns:
@@ -103,7 +117,7 @@ class _ArboristToTemplate:
             if 'Level {} metadata value'.format(level) not in self.df.columns:
                 self.df['Level {} metadata value'.format(level)] = np.nan
 
-    def add_metadata_to_df(self, metadata, row_index, level):
+    def _add_metadata_to_df(self, metadata, row_index, level):
         for item in metadata:
             tag = item[0]
             value = item[1][0]
@@ -111,7 +125,7 @@ class _ArboristToTemplate:
             self.df.loc[row_index, 'Level {} metadata value'.format(level)] = value
             row_index += 1
 
-    def determine_row_index(self, last_row_index, cols_equal_or_higher_level, last_regular_node_row_index):
+    def _determine_row_index(self, last_row_index, cols_equal_or_higher_level, last_regular_node_row_index):
         """Determine the row index on which the current node should be inserted."""
         # The df is still empty
         if last_row_index is None:
@@ -124,35 +138,43 @@ class _ArboristToTemplate:
             row_index = last_row_index + 1
         return row_index
 
-    def insert_node_data_into_df(self, node, depth):
+    def _insert_node_data_into_df(self, node, depth):
         last_row_index = max(self.df.apply(pd.Series.last_valid_index), default=None)
         regular_node_cols = [col for col in self.df.columns if 'metadata' not in col]
         last_regular_node_row_index = max(self.df[regular_node_cols].apply(pd.Series.last_valid_index), default=None)
         cols_equal_or_higher_level = [col for col in self.df.columns if int(col.split()[1]) >= depth]
 
-        row_index = self.determine_row_index(last_row_index, cols_equal_or_higher_level, last_regular_node_row_index)
+        row_index = self._determine_row_index(last_row_index, cols_equal_or_higher_level, last_regular_node_row_index)
         self.df.loc[row_index, 'Level {}'.format(depth)] = node['text']
 
         if 'metadata' in node:
-            self.add_metadata_to_df(node['metadata'], row_index, depth)
+            self._add_metadata_to_df(node['metadata'], row_index, depth)
 
-    def add_metadata_to_node_level(self, node):
-        """Add metadata to actual node instead of it being a child node"""
+    def _add_metadata_to_node_level(self, node):
+        """Move metadata to actual node instead of it being a child node"""
         if any([child_node['text'] == 'Tags' for child_node in node['children']]):
             node['metadata'] = [child['data']['tags'] for child in node['children'] if child['text'] == 'Tags'][0]
             # Sort tags by weight
             node['metadata'] = sorted(node['metadata'].items(), key=lambda kv: int(kv[1][1]))
 
-    def loop_through_nodes(self, node, depth=1):
+    def _loop_through_nodes(self, node, depth=1):
+        """
+        Main recursive function that loops through all
+        the JSON tree nodes and adds them to the tree
+        template df
+        :param node: concept node as present in the JSON tree
+        :param depth: recursive depth (i.e tree level #)
+        :return: None
+        """
         if not self.skip_metadata:
-            self.add_metadata_to_node_level(node)
+            self._add_metadata_to_node_level(node)
         # Skip tag nodes and categorical values
         if node['text'] in ['Tags', 'SUBJ_ID'] or node.get('type') == 'alpha':
             return
-        self.insert_node_data_into_df(node, depth)
+        self._insert_node_data_into_df(node, depth)
 
         for child_node in node['children']:
-            self.loop_through_nodes(child_node, depth+1)
+            self._loop_through_nodes(child_node, depth + 1)
 
 
 @click.command()
@@ -163,7 +185,7 @@ class _ArboristToTemplate:
               help='Ignore all metadata in the tree file')
 def _main(tree_input, output_dir, no_metadata):
     """1. Either a path to a treefile or an Arborist URL of a tree."""
-    arborist_to_tree_template(tree_input, output_dir, no_metadata)
+    arborist_to_tree_template(tree_input, output_dir, no_metadata, write_immediately=True)
 
 
 if __name__ == "__main__":
