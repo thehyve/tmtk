48e54239e5b2f8230910980c34be221e755627a5
jochemb <j.bijlard@gmail.com>
2018-08-23 14:11:38 +0200

    Bump to 0.5.3

--- tmtk/version.py
+++ tmtk/version.py
@@ -4,5 +4,5 @@ This file is distributed under the GNU General Public License
   (see accompanying file LICENSE).
 """
 
-version_info = (0, 5, 3, 'pre')
+version_info = (0, 5, 3)
 __version__ = '.'.join(map(str, version_info))
