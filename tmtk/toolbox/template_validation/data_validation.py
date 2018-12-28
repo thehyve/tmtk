3b38406a8712d0dbd59608724b1a884e5a455fa9
BrendaHijmans <42644481+BrendaHijmans@users.noreply.github.com>
2018-12-28 18:01:04 +0100

    added checks for duplicate and mandatory col and col mapping in tree (#63)
    * added checks for duplicate and mandatory col and col mapping in tree
    
    * changes according to review
    
    * changes to method to detect missing column names in tree sheet
    
    * changed col in tree sheet check to check per data source
    
    * refactor data source column validation and update docstring

--- tmtk/toolbox/template_validation/data_validation.py
+++ tmtk/toolbox/template_validation/data_validation.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(' Clinical data')
 
 class DataValidator:
 
-    def __init__(self, data_df, tree_df, sheet_name):
+    def __init__(self, data_df, tree_df, data_source):
         """Creates object DataValidator that runs validation tests on clinical data and gives user-friendly
         error messages.
 
@@ -18,11 +18,14 @@ class DataValidator:
         """
         self.data_df = data_df
         self.tree_df = tree_df
-        self.sheet_name = sheet_name
+        self.data_source = data_source
         self.n_comment_lines = 0
         self.is_valid = True
         self.tests_to_run = [self.after_comments(),
-                             self.check_encoding()
+                             self.check_encoding(),
+                             self.mandatory_col(),
+                             self.unique_col_names(),
+                             self.col_name_in_tree_sheet()
                              ]
 
     def after_comments(self):
@@ -47,11 +50,46 @@ class DataValidator:
             for idx, value in series.iteritems():
                 if not is_utf8(value):
                     logger.error(" Value in '{}' at column '{}', row: {} cannot be UTF-8 "
-                                 "encoded.".format(self.sheet_name, col_name, idx + 1))
+                                 "encoded.".format(self.data_source, col_name, idx + 1))
                 if any((c in forbidden_chars) for c in value):
                     self.is_valid = False
                     logger.error(" Detected '#' or '\\' in '{}' at column: '{}', row: "
-                                 "{}.".format(self.sheet_name, col_name, idx + 1))
+                                 "{}.".format(self.data_source, col_name, idx + 1))
+
+    def mandatory_col(self):
+        """Set self.is_valid = False if a subject identifier column called 'SUBJ_ID' is
+        not present in the clinical data and give an error message if it is not.
+        """
+        if 'SUBJ_ID' not in self.data_df.columns:
+            self.is_valid = False
+            logger.error(" Mandatory column containing subject identifiers (SUBJ_ID) not detected "
+                         "in clinical data '{}'.".format(self.data_source))
+
+    def unique_col_names(self):
+        """Set self.is_valid = False if a double column name is found and give an
+        error message specifying the duplicate column name(s).
+        """
+        columns = self.data_df.columns
+        duplicate_columns = set(columns[columns.duplicated()])
+
+        if duplicate_columns:
+            self.is_valid = False
+            logger.error(" Detected duplicate column name(s) in '" + self.data_source + "': \n\t" +
+                         "\n\t".join(duplicate_columns))
+
+    def col_name_in_tree_sheet(self):
+        """Check whether all the columns for the current data source are present in the tree sheet.
+        If data source contains additional columns: Log a warning message with all those column names.
+        """
+        # All column names present in the tree sheet for the current data source
+        tree_columns = self.tree_df[self.tree_df['Sheet name/File name'] == self.data_source]['Column name'].tolist()
+
+        missing_columns = [col for col in self.data_df.columns if col != 'SUBJ_ID' and col not in tree_columns]
+
+        if missing_columns:
+            logger.warning(" The following column(s) in '" + self.data_source +
+                           "' are not listed in Tree structure column: 'Column name': "
+                           "\n\t" + "\n\t".join(missing_columns))
 
 
 def is_utf8(string):
