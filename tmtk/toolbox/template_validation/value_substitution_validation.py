40fe5d1c540d4b9756f3c9b3dc9550c97c3c3988
BrendaHijmans <42644481+BrendaHijmans@users.noreply.github.com>
2019-01-11 17:04:20 +0100

    added validation for value substitution: mandatory cols, checks for d… (#64)
    * added validation for value substitution: mandatory cols, checks for data in cols
    
    * added super class Validator and restructured template_validation.py

--- tmtk/toolbox/template_validation/value_substitution_validation.py
+++ tmtk/toolbox/template_validation/value_substitution_validation.py
@@ -1,19 +1,27 @@
 import logging
 
-logger = logging.getLogger(" Value substitution sheet")
-logger.setLevel(logging.DEBUG)
+from .validation import Validator
 
 
-class ValueSubstitutionValidator:
+class ValueSubstitutionValidator(Validator):
 
-    def __init__(self, value_substitution_df, source_dir, template):
+    def __init__(self, df, source_dir, template):
         """Creates object ValueSubstitutionValidator that runs validation tests on value substitution sheet and
         gives user-friendly error messages.
 
-        :param self.value_substitution_df: value substitution sheet in pandas data frame
-        :param self.is_valid: boolean tracking if data passes validation steps
-        :param self.tests_to_run: dict containing function calls for data validation tests
+        :param self.source_dir: directory containing the template and possible other source files
+        :param self.template: the loaded template
+        :param self.tests_to_run: list containing function calls for data validation tests
         """
-        self.value_substitution_df = value_substitution_df
-        self.is_valid = True
-        self.tests_to_run = {}
+        super().__init__(df)
+        self.logger = logging.getLogger(" Value substitution sheet")
+        self.source_dir = source_dir
+        self.template = template
+        self.mandatory_columns = ['Sheet name/File name', 'Column name', 'From value', 'To value']
+        self.tests_to_run = (test for test in
+                             [self.after_comments,
+                              self.check_mandatory_columns,
+                              self.check_forbidden_chars,
+                              ])
+
+        self.validate_sheet()
