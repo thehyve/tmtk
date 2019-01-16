0ae5bf9af3125c3c7eb0e3272449d5a477467a73
BrendaHijmans <brenda.hijmans@gmail.com>
2019-01-16 12:43:35 +0100

    added checks for unique value subst and and presence of data source

--- tmtk/toolbox/template_validation/value_substitution_validation.py
+++ tmtk/toolbox/template_validation/value_substitution_validation.py
@@ -1,4 +1,7 @@
 import logging
+import pandas as pd
+import os
+import csv
 
 from .validation import Validator
 
@@ -22,6 +25,42 @@ class ValueSubstitutionValidator(Validator):
                              [self.after_comments,
                               self.check_mandatory_columns,
                               self.check_forbidden_chars,
+                              self.value_substitution_unique,
+                              self.check_data_source,
                               ])
 
         self.validate_sheet()
+
+    def value_substitution_unique(self):
+        """ Checks whether the combination of Sheet name/File name and Column name is unique.
+        """
+        self.df['Combo'] = tuple(zip(self.df['Sheet name/File name'], self.df['Column name'], self.df['From value']))
+
+        unique = []
+        for counter, combo in enumerate(self.df['Combo'], start=self.n_comment_lines + 1):
+            if combo in unique:
+                self.is_valid = False
+                self.logger.error(" Row {} contains a duplicate value substitution.".format(counter + 1))
+            else:
+                unique.append(combo)
+
+    def check_data_source(self):
+        """ Checks whether data source referenced in 'Sheet name/File name' is filled out and present in source_dir.
+        """
+        files_no_ext = [".".join(file.split(".")[:-1]) for file in os.listdir(self.source_dir)]
+
+        for counter, data_source in enumerate(self.df['Sheet name/File name'], start=self.n_comment_lines + 1):
+            if pd.isnull(data_source):
+                self.is_valid = False
+                self.can_continue = False
+                self.logger.error(" No data source referenced at row {} in column 'Sheet name/File name'"
+                                  .format(counter + 1))
+            else:
+                if data_source.rsplit('.')[0] not in self.template.sheet_names and data_source.rsplit('.')[0] not in \
+                        files_no_ext:
+                    self.is_valid = False
+                    self.can_continue = False
+                    self.logger.error(" Clinical data in sheet or file '{}' not found. Check whether it is correctly "
+                                      "referenced in column 'Sheet name/File name and is a sheet in the template or a "
+                                      "file stored in the same directory as the template."
+                                      .format(data_source.rsplit('.')[0]))
