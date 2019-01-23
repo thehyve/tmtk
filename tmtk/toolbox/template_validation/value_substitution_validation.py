a5d6e6c32c738ac2e96fa44b772954f4d9071982
BrendaHijmans <brenda.hijmans@gmail.com>
2019-01-23 12:56:20 +0100

    adjustments according to comments

--- tmtk/toolbox/template_validation/value_substitution_validation.py
+++ tmtk/toolbox/template_validation/value_substitution_validation.py
@@ -1,7 +1,4 @@
 import logging
-import pandas as pd
-import os
-import csv
 
 from .validation import Validator
 
@@ -12,8 +9,10 @@ class ValueSubstitutionValidator(Validator):
         """Creates object ValueSubstitutionValidator that runs validation tests on value substitution sheet and
         gives user-friendly error messages.
 
+        :param self.logger: logger to log errors
         :param self.source_dir: directory containing the template and possible other source files
         :param self.template: the loaded template
+        :param self.mandatory_columns: mandatory columns for this sheet
         :param self.tests_to_run: list containing function calls for data validation tests
         """
         super().__init__(df)
@@ -25,77 +24,65 @@ class ValueSubstitutionValidator(Validator):
                              [self.after_comments,
                               self.check_mandatory_columns,
                               self.check_forbidden_chars,
-                              self.value_substitution_unique,
                               self.no_empty_cells,
-                              self.check_source_validity,
+                              self.value_substitution_unique,
+                              self.source_in_tree,
                               ])
 
         self.validate_sheet()
+
+    def no_empty_cells(self):
+        """ Checks whether no cells are left empty.
+        """
+        empty_cells = self.df[self.df.isnull().any(axis=1)]
+        empty_cells.index += 1
+
+        index_empty_cells = [str(idx) for idx in empty_cells.index]
+
+        if index_empty_cells:
+            self.is_valid = False
+            self.logger.error(' The following row(s) contain empty cells: \n\t' + '\n\t'.join(index_empty_cells))
 
     def value_substitution_unique(self):
         """ Checks whether the combination of the columns 'Sheet name/File name', 'Column name' and 'From value'
         is unique.
         """
-        self.df['column_combo'] = tuple(zip(self.df['Sheet name/File name'], self.df['Column name'],
-                                            self.df['From value']))
-        duplicate_rows = self.df[self.df.duplicated(subset='column_combo', keep=False)]
+        duplicate_rows = self.df[self.df.duplicated(subset=['Sheet name/File name', 'Column name', 'From value'],
+                                                    keep=False)]
         duplicate_rows.index += 1
 
         index_duplicate_rows = [str(idx) for idx in duplicate_rows.index]
-
         if index_duplicate_rows:
             self.is_valid = False
             self.logger.error(' The following rows contain a duplicate value substitution: \n\t' + '\n\t'
                               .join(index_duplicate_rows))
 
-    def no_empty_cells(self):
-        """ Checks whether no cells are left empty.
+    def source_in_tree(self):
+        """ Checks whether data source referenced in 'Sheet name/File name' and corresponding column names are present
+        in the tree structure sheet.
         """
-        for col_name, series in self.df.iteritems():
-            for idx, value in series.iteritems():
-                if pd.isnull(value):
-                    self.is_valid = False
-                    self.can_continue = False
-                    self.logger.error(" Column '{}', row  '{}' should not be empty".format(col_name, idx + 1))
+        self.df = self.df.dropna(subset=['Sheet name/File name', 'Column name']).reset_index(drop=True)
+        incorrect_source_reference = set()
 
-    def check_source_validity(self):
-        """ Checks whether data source referenced in 'Sheet name/File name' is filled out and present in source_dir.
-        """
-        files = [file for file in os.listdir(self.source_dir)]
-        files_no_ext = [".".join(file.split(".")[:-1]) for file in os.listdir(self.source_dir)]
+        for sheet in self.template.sheet_names:
+            if sheet.lower() == 'tree structure':
+                tree_df = self.template.parse(sheet, comment='#')
 
-        for counter, data_source in enumerate(self.df['Sheet name/File name'], start=self.n_comment_lines + 1):
+        for counter, data_source in enumerate(self.df['Sheet name/File name']):
             data_source = data_source.rsplit('.')[0]
             column = self.df['Column name'][counter]
+            tree_columns = tree_df[tree_df['Sheet name/File name'] == data_source]['Column name'].tolist()
 
-            if data_source not in self.template.sheet_names and data_source not in files_no_ext:
-                self.is_valid = False
-                self.can_continue = False
-                self.logger.error(" Clinical data in sheet or file '{}' not found. Check whether the reference in "
-                                  "column 'Sheet name/File name' is correct and is a sheet in the template or a file "
-                                  "stored in the same directory as the template.".format(data_source))
+            if data_source in tree_df['Sheet name/File name'].values:
+                if column not in tree_columns:
+                    self.is_valid = False
+                    self.logger.error(
+                        " Column '{}' in combination with sheet or file named '{}' not found in tree structure. Check "
+                        "whether the reference is correct.".format(column, data_source, column))
             else:
-                if data_source in self.template.sheet_names:
-                    data_df = self.template.parse(data_source, comment='#')
-                    column_in_data(self, column, data_df, data_source)
-                else:
-                    file_idx = files_no_ext.index(data_source)
-                    if files[file_idx].rsplit('.')[1] in ['xls', 'xlsx']:
-                        data_df = pd.read_excel(os.path.join(self.source_dir, files[file_idx]), comment='#')
-                        column_in_data(self, column, data_df, data_source)
-                    else:
-                        try:
-                            data_df = pd.read_csv(os.path.join(self.source_dir, files[file_idx]), dtype='str', sep=None,
-                                                  engine='python')
-                            column_in_data(self, column, data_df, data_source)
-                        except csv.Error:
-                            self.logger.error(" Clinical data file '{}' cannot be read. It may not be a flat text file."
-                                              .format(files[file_idx]))
+                incorrect_source_reference.add(data_source)
 
-
-def column_in_data(self, column, data_df, data_source):
-    """
-    """
-    if column not in data_df.columns:
-        self.is_valid = False
-        self.logger.error(" Column: '{}' not found in sheet or file: '{}".format(column, data_source))
+        if incorrect_source_reference:
+            self.is_valid = False
+            self.logger.error(" Sheet or file named '{}' not found in tree structure. Check whether the reference "
+                              "is correct.".format(data_source))
