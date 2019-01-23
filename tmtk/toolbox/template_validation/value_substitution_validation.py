eb754ac6a2fdef09b60beb43d08313eb937bc4b9
BrendaHijmans <brenda.hijmans@gmail.com>
2019-01-23 16:11:24 +0100

    changes to reading tree_df and value_subst_validation

--- tmtk/toolbox/template_validation/value_substitution_validation.py
+++ tmtk/toolbox/template_validation/value_substitution_validation.py
@@ -5,20 +5,19 @@ from .validation import Validator
 
 class ValueSubstitutionValidator(Validator):
 
-    def __init__(self, df, source_dir, template):
+    def __init__(self, df, template, tree_df):
         """Creates object ValueSubstitutionValidator that runs validation tests on value substitution sheet and
         gives user-friendly error messages.
 
         :param self.logger: logger to log errors
-        :param self.source_dir: directory containing the template and possible other source files
         :param self.template: the loaded template
         :param self.mandatory_columns: mandatory columns for this sheet
         :param self.tests_to_run: list containing function calls for data validation tests
         """
         super().__init__(df)
         self.logger = logging.getLogger(" Value substitution sheet")
-        self.source_dir = source_dir
         self.template = template
+        self.tree_df = tree_df
         self.mandatory_columns = ['Sheet name/File name', 'Column name', 'From value', 'To value']
         self.tests_to_run = (test for test in
                              [self.after_comments,
@@ -61,24 +60,22 @@ class ValueSubstitutionValidator(Validator):
         """ Checks whether data source referenced in 'Sheet name/File name' and corresponding column names are present
         in the tree structure sheet.
         """
-        self.df = self.df.dropna(subset=['Sheet name/File name', 'Column name']).reset_index(drop=True)
+        df = self.df.dropna(subset=['Sheet name/File name', 'Column name']).reset_index(drop=True)
         incorrect_source_reference = set()
 
         for sheet in self.template.sheet_names:
             if sheet.lower() == 'tree structure':
                 tree_df = self.template.parse(sheet, comment='#')
 
-        for counter, data_source in enumerate(self.df['Sheet name/File name']):
-            data_source = data_source.rsplit('.')[0]
-            column = self.df['Column name'][counter]
+        for counter, data_source in enumerate(df['Sheet name/File name']):
+            column = df['Column name'][counter]
             tree_columns = tree_df[tree_df['Sheet name/File name'] == data_source]['Column name'].tolist()
 
             if data_source in tree_df['Sheet name/File name'].values:
                 if column not in tree_columns:
                     self.is_valid = False
-                    self.logger.error(
-                        " Column '{}' in combination with sheet or file named '{}' not found in tree structure. Check "
-                        "whether the reference is correct.".format(column, data_source, column))
+                    self.logger.error(" Column '{}' in combination with sheet or file named '{}' not found in tree "
+                                      "structure. Check whether the reference is correct.".format(column, data_source))
             else:
                 incorrect_source_reference.add(data_source)
 
